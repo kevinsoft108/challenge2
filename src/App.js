c3d6ed7c3e0c14358db67883af7b66824e66f67c
kevinsoft108 <kevinsoft108@gmail.com>
2022-05-18 23:01:27 +0700

    Complete mock file upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './form.css';
 
 function App() {
+  const [files, setFiles] = useState([]);
+
+  function onChangeFile(e) {
+    var selected = [];
+    for (const file of e.target.files) {
+      var errors = [];
+      if (file.type !== 'application/pdf') {
+        errors.push('Invalid PDF file.');
+      }
+
+      if (file.size > 10 * 1024 * 1024) {
+        errors.push('File size should be less than 10MB.')
+      }
+
+      file.errors = errors.join(' ');
+      selected.push(file);
+    };
+    setFiles(selected);
+  }
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    alert('File saved successfully.');
+    setFiles([]);
+  }
+
+  function deleteFile(index) {
+    var copy = [...files];
+    copy.splice(index, 1);
+    setFiles(copy);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onFormSubmit}>
+        <h1>File Upload</h1>
+        <span className="upload-wrapper">
+          <button>Select PDF</button>
+          <input type="file" multiple={true} onChange={onChangeFile} />
+        </span>
+        <button type="submit">Save</button>
+      </form>
+      <table>
+        <tbody>
+          {files.map((file, i) => (
+              <tr key={i}>
+                <td>{file.name}</td>
+                <td>{file.size}</td>
+                <td>{file.errors}</td>
+                <td><button onClick={() => deleteFile(i)}>Delete</button></td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
